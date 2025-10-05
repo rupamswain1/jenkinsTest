960bc4a330aec25e67d7eeaefce5a60fb8901c88
Rupam Swain <rupamswain1@gmail.com>
2025-10-05 22:09:26 +0530

    Update Calculator.java
    multiply and divide

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,3 @@
-
 public class Calculator {
 
     public void add(int a, int b){
@@ -8,11 +7,20 @@ public class Calculator {
         System.out.println(a-b);
     }
 
+    public void multiply(int a, int b){
+        System.out.println(a*b);
+    }
+
+    public void divide(int a, int b){
+        System.out.println((float)a/b);
+    }
     public static void main(String[] args){
         int a = 10;
         int b = 20;
         Calculator calculator = new Calculator();
         calculator.add(a,b);
         calculator.sub(a,b);
+        calculator.multiply(a,b);
+        calculator.divide(a,b);
     }
 }
